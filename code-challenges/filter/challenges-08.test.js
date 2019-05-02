fd88741f5f6478467a51ff05908c76e363e6190c
emd5 <elizabeth.mahoney.dao@gmail.com>
2019-05-01 20:23:38 -0700

    completed

--- code-challenges/filter/challenges-08.test.js
+++ code-challenges/filter/challenges-08.test.js
@@ -168,7 +168,8 @@ For example: evenOddNumericValues(['Gregor', 2, 4, 1]) returns ['even', 'even', 
 ------------------------------------------------------------------------------------------------ */
 
 const evenOddNumericValues = (arr) => {
-  // Solution code here...
+  return arr.filter( element => typeof element !== 'string' ).map( element => element % 2 === 0 ? 'even' : 'odd' );
+
 };
 
 /* ------------------------------------------------------------------------------------------------
