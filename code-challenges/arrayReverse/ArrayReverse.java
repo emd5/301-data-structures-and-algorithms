3e492ce6f2594c65f6d8b85811b944e97e47cb8e
emd5 <elizabeth.mahoney.dao@gmail.com>
2019-05-24 09:10:32 -0700

    refactored code

--- code-challenges/arrayReverse/ArrayReverse.java
+++ code-challenges/arrayReverse/ArrayReverse.java
@@ -22,8 +22,8 @@ public class ArrayReverse {
 
         for(int i=0; i< inputArray.length/2; i++){
             int temp = inputArray[i];
-            inputArray[i] = inputArray[inputArray.length - 1 - i];
-            inputArray[inputArray.length - 1 - i] = temp;
+            inputArray[i] = inputArray[(inputArray.length - 1) - i];
+            inputArray[(inputArray.length - 1) - i] = temp;
         }
 
         System.out.println("New Array is:");
